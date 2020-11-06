c583640758b94e7ec05339645c54b6da001e1127
Kevin Bates <kbates4@gmail.com>
2020-11-06 14:14:57 -0800

    Introduce documentation framework

--- setup.py
+++ setup.py
@@ -1,26 +1,42 @@
 
 import pathlib
 from setuptools import setup, find_packages
+from jupyter_packaging import get_version
 
-here = pathlib.Path('.')
+NAME = "pytest-jupyter"
+PACKAGE_NAME = NAME.replace("-", "_")
+DESCRIPTION = "A pytest plugin for testing Jupyter libraries and extensions."
+VERSION = get_version(f"{PACKAGE_NAME}/_version.py")
 
-readme_path = here.joinpath('README.md')
+HERE = pathlib.Path('.')
+readme_path = HERE.joinpath('README.md')
 README = readme_path.read_text()
-VERSION = "0.0.1"
 
-setup(
-    name="pytest-jupyter",
-    packages=find_packages(),
+setup_args = dict(
+    name=NAME,
     version=VERSION,
-    description="A pytest plugin for testing Jupyter core libraries and extensions.",
+    description=DESCRIPTION,
     long_description=README,
     long_description_content_type='text/markdown',
+    packages=find_packages(),
     author='Jupyter Development Team',
     author_email='jupyter@googlegroups.com',
     url='http://jupyter.org',
     license='BSD',
     platforms="Linux, Mac OS X, Windows",
     keywords=['Jupyter', 'pytest'],
+    install_requires=[
+        'traitlets',
+        'tornado',
+        'pytest',
+        'nbformat',
+        'pytest-tornasync',
+        'jupyter_core',
+        'jupyter_server'
+    ],
+    extras_require={
+        'docs': ['Sphinx', 'myst_parser', 'pydata_sphinx_theme']
+    },
     # the following makes a plugin available to pytest
     entry_points={
         "pytest11": [
@@ -31,3 +47,8 @@ setup(
     # custom PyPI classifier for pytest plugins
     classifiers=["Framework :: Pytest"],
 )
+
+
+if __name__ == '__main__':
+    setup(**setup_args)
+
