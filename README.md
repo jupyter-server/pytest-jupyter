5b3ada2b71269c4ed04bda7b83ab992904db9143
Afshin T. Darian <git@darian.af>
2020-12-09 16:44:01 +0000

    Remove entry points

--- README.md
+++ README.md
@@ -1,3 +1,39 @@
 # pytest-jupyter
 
 A set of pytest plugins for Jupyter libraries and extensions.
+
+## Basic Usage
+
+First, install `pytest-jupyter` from PyPI using pip:
+```bash
+pip install pytest-jupyter
+```
+This installs the basic pytest-jupyter package that insludes fixture definitions for the various Jupyter-based pytest plugins.
+
+To use one of these plugins, you'll also need to install their dependencies. This requires a second `pip install` call. For example, if you'd like to use the `jupyter_server` plugin, you'll need to call:
+```bash
+pip install "pytest-jupyter[server]"
+```
+This *should* install everything you need for the plugin to work.
+
+To use a plugin, add it to the `pytest_plugins` list in the `conftest.py` of your project's root test directory.
+```python
+# inside the conftest.py
+
+pytest_plugins = ["pytest_jupyter.jupyter_server"]
+```
+All fixtures inside the plugin (e.g. jupyter_server) will be available to all of your project's unit tests. You can use a fixtures by passing it as an argument to your unit test function:
+```python
+async def test_jupyter_server_api(jp_fetch):
+    # Send request to a temporary Jupyter Server Web Application
+    response = await jp_fetch("api/spec.yml")
+
+    # Confirm that the request is successful.
+    assert response.code == 200
+```
+
+You can list the fixtures for a given plugin using the `--fixtures` argument from the pytest command line interface:
+```bash
+pytest --fixtures -p pytest_jupyter.jupyter_server
+```
+or by calling the `pytest --fixtures` where the plugin is listed in the `pytest_plugins` variable of a given test directory.
--- setup.py
+++ setup.py
@@ -12,38 +12,39 @@ HERE = pathlib.Path('.')
 readme_path = HERE.joinpath('README.md')
 README = readme_path.read_text()
 
+# Build the extra requirements for each plugin.
+EXTRA_REQUIRES = {
+    'docs': [
+        'Sphinx',
+        'myst_parser',
+        'pydata_sphinx_theme',
+    ],
+    'core': [
+        'jupyter_core'
+    ],
+    'server': [
+        'nbformat',
+        'jupyter_core',
+        'jupyter_server',
+        'pytest-tornasync'
+    ]
+}
+
 setup_args = dict(
     name=NAME,
     version=VERSION,
     description=DESCRIPTION,
     long_description=README,
     long_description_content_type='text/markdown',
-    packages=find_packages(),
+    packages=find_packages('.', exclude=['tests', 'tests.*']),
     author='Jupyter Development Team',
     author_email='jupyter@googlegroups.com',
     url='http://jupyter.org',
     license='BSD',
     platforms="Linux, Mac OS X, Windows",
     keywords=['Jupyter', 'pytest'],
-    install_requires=[
-        'traitlets',
-        'tornado',
-        'pytest',
-        'nbformat',
-        'pytest-tornasync',
-        'jupyter_core',
-        'jupyter_server'
-    ],
-    extras_require={
-        'docs': ['Sphinx', 'myst_parser', 'pydata_sphinx_theme']
-    },
-    # the following makes a plugin available to pytest
-    entry_points={
-        "pytest11": [
-            "pytest-jupyterserver = pytest_jupyter.jupyter_server",
-            "pytest-jupytercore = pytest_jupyter.jupyter_core"
-        ]
-    },
+    install_requires=['pytest'],
+    extras_require=EXTRA_REQUIRES,
     # custom PyPI classifier for pytest plugins
     classifiers=["Framework :: Pytest"],
 )
