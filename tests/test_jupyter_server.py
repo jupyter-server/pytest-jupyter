8b7e6eec8e5d65763dacdb688967045e6edf808f
Steven Silvester <steven.silvester@ieee.org>
2023-11-22 21:47:09 -0600

    fix test

--- tests/test_jupyter_server.py
+++ tests/test_jupyter_server.py
@@ -23,12 +23,12 @@ def test_skip(jp_serverapp):
 
 async def test_get_api_spec(jp_fetch):
     response = await jp_fetch("api", "spec.yaml", method="GET")
-    assert response.code == HTTPStatus.ACCEPTED
+    assert response.code == HTTPStatus.OK
 
 
 async def test_send_request(send_request):
     code = await send_request("api/spec.yaml", method="GET")
-    assert code == HTTPStatus.ACCEPTED
+    assert code == HTTPStatus.OK
 
 
 async def test_connection(jp_fetch, jp_ws_fetch, jp_http_port, jp_auth_header):
