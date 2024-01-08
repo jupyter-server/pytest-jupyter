52127908452e9df283d6a957a1f65103815f2346
Steven Silvester <steven.silvester@ieee.org>
2024-01-07 20:28:43 -0600

    another windows fix

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,10 @@
+import asyncio
 import os
+import sys
+
+# For backwards compatibility with older versions of client and server.
+if sys.platform == "win32":
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
 os.environ["JUPYTER_PLATFORM_DIRS"] = "1"
 pytest_plugins = [
